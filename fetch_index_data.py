3622dceab3682a510dea9c6fa9bbff4d3fabca78
Edwin Brown <teddy.brown@ischool.berkeley.edu>
2021-04-06 07:03:33 -0700

    Add one more year to the data

--- fetch_index_data.py
+++ fetch_index_data.py
@@ -16,18 +16,18 @@ def getIndexCSVs():
     """
     # Download DJII data at 1m intervals
     djii1m_df = yf.download(
-        "^DJI", start="1987-01-01", end="2020-01-01", interval="1mo"
+        "^DJI", start="1987-01-01", end="2021-01-01", interval="1mo"
     )
     djii1m_df.to_csv("djii_1m.csv")
 
     # Download S&P500 data at 1m intervals
     sp5001m_df = yf.download(
-        "^GSPC", start="1987-01-01", end="2020-01-01", interval="1mo"
+        "^GSPC", start="1987-01-01", end="2021-01-01", interval="1mo"
     )
     sp5001m_df.to_csv("sp500_1m.csv")
 
     # Download Nasdaq data at 1m intervals
-    nd1m_df = yf.download("^IXIC", start="1987-01-01", end="2020-01-01", interval="1mo")
+    nd1m_df = yf.download("^IXIC", start="1987-01-01", end="2021-01-01", interval="1mo")
     nd1m_df.to_csv("nasdaq_1m.csv")
 
 
